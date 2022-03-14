f5dc0d9c43685b68f282265fb7bc728b23ebc0eb
Owen <EwonRael@yahoo.com>
2022-03-14 23:27:38 +0100

    Add files via upload

--- portfolio/script.js
+++ portfolio/script.js
@@ -3,3 +3,8 @@ function randStyle(me){
   const smallcaps = ["small-caps", "normal", "normal"];
   me.style = "font-variation-settings: \'wght\' " + 100 * Math.floor(Math.random() * 8)  + "; font-style: " + italic[Math.floor(Math.random() * 2)] + "; font-variant: " + smallcaps[Math.floor(Math.random() * 3)] + ";"
 }
+
+function showme(){
+  document.getElementById("blackout").style = "background-color: white;"
+  document.getElementById("blackout2").style = "background-color: white;"
+}
