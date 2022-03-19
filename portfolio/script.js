1b1b35ce728331a5a85b0061183d09f069687d8d
Owen <EwonRael@yahoo.com>
2022-03-19 12:38:31 +0100

    Add files via upload

--- portfolio/script.js
+++ portfolio/script.js
@@ -8,7 +8,7 @@ function showme(){
   document.getElementById("blackout").style = "background-color: white; cursor: default;"
   document.getElementById("blackout2").innerHTML = "As the podcast develops things start to become stranger and stranger. It seems as though the lack of information on this art movement isn't just because it is lost to time, but due to an active conspiracy involving the US Government. No one wants to talk, and when they do their stories don't add up. What happened to Lynda Alden? What does her untimely death have to do with this conspiracy? Are we in danger?"
   document.getElementById("blackout2").style = "background-color: white; cursor: default;"
-  document.getElementById("blackout").innerHTML = "Eventually things escalate so much is becomes obvious that the podcast, which purported to be true, is actually a work of fiction. There never was an art movement called \"Trickism.\" Lynda Alden never existed. It, itself is a work of Trickism. The podcast spends a great deal of time explaining exactly how it's own mechanisms of trickery work, and it still succeeds in tricking you in the end."
+  document.getElementById("blackout").innerHTML = "Eventually things escalate so much it becomes obvious that the podcast, which purported to be true, is actually a work of fiction. There never was an art movement called \"Trickism.\" Lynda Alden never existed. It, itself is a work of Trickism. The podcast spends a great deal of time explaining exactly how it's own mechanisms of trickery work, and it still succeeds in tricking you in the end."
 }
 
 function tweetRand(){
