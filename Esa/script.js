03bbdd98b7d62699e5fec574ea87c58b897baf14
Owen <EwonRael@yahoo.com>
2023-11-29 17:02:37 -0600

    Add files via upload

---
+++ Esa/script.js
@@ -0,0 +1,6 @@
+onmousemove = function(mouse){
+	let w = (mouse.clientX/window.innerWidth)*800 + 100
+	let h = (mouse.clientY/window.innerHeight)*135 + 9
+	let s = ((mouse.clientX/window.innerWidth)*0.7 + 0.8)*(1 - (mouse.clientY/(window.innerHeight*2)))
+	document.getElementById('title').style = "font-variation-settings: 'opsz' " + h + ", 'SOFT' 100, 'wght' " + w + "; letter-spacing: -"+s+"vw"
+}
