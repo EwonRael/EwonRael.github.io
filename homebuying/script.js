9b470854df83153b7c7da5e27f2af1f28416e7be
Owen <EwonRael@yahoo.com>
2024-04-22 17:47:49 -0500

    Add files via upload

---
+++ homebuying/script.js
@@ -0,0 +1,39 @@
+function makeNumber(x) {
+	x = x.replace(/\D/g, "")
+	if (x > 999999) {
+		x = [x.slice(0, -3), ",", x.slice(-3)].join('')
+		x = [x.slice(0, -7), ",", x.slice(-7)].join('')
+	}
+	else if (x > 999) {
+		x = [x.slice(0, -3), ",", x.slice(-3)].join('')
+	}
+	document.getElementById("income").value = x
+}
+
+function makeCredit(x) {
+	x = x.replace(/\D/g, "")
+	if (x > 850) {
+		x = 850
+	}
+	document.getElementById("credit").value = x
+}
+
+function makeMonth(x) {
+	x = x.replace(/\D/g, "")
+	if (x > 999999) {
+		x = [x.slice(0, -3), ",", x.slice(-3)].join('')
+		x = [x.slice(0, -7), ",", x.slice(-7)].join('')
+	}
+	else if (x > 999) {
+		x = [x.slice(0, -3), ",", x.slice(-3)].join('')
+	}
+	document.getElementById("month").value = x
+}
+
+function familySize(x) {
+	x = x.replace(/\D/g, "")
+	if (x > 850) {
+		x = 850
+	}
+	document.getElementById("familySize").value = x
+}
