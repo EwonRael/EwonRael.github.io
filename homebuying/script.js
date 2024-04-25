6af3266452814dda6f6f04ed373968f8310b50b2
Owen <EwonRael@yahoo.com>
2024-04-24 21:10:37 -0500

    Add files via upload

--- homebuying/script.js
+++ homebuying/script.js
@@ -37,3 +37,21 @@ function familySize(x) {
 	}
 	document.getElementById("familySize").value = x
 }
+
+function makeNumberTwo(x) {
+	x = x.replace(/\D/g, "")
+	if (x > 999999) {
+		x = [x.slice(0, -3), ",", x.slice(-3)].join('')
+		x = [x.slice(0, -7), ",", x.slice(-7)].join('')
+	}
+	else if (x > 999) {
+		x = [x.slice(0, -3), ",", x.slice(-3)].join('')
+	}
+	document.getElementById("downpayment").value = x
+}
+
+function showPrograms() {
+	document.getElementById("questions").style = "display: none;"
+	document.getElementById("programs").style = "display: block;"
+	window.scrollTo(0, 0);
+}
